d6ec3bbef6c5fa298e3a78138bab2680e1094d01
jgravalo <jesusgrawork361@gmail.com>
2023-08-26 20:15:12 +0200

    algunas funciones partidas y/o acortadas para entrar en la norminette

--- src/meta.c
+++ src/meta.c
@@ -6,7 +6,7 @@
 /*   By: theonewhoknew <theonewhoknew@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 17:38:46 by theonewhokn       #+#    #+#             */
-/*   Updated: 2023/08/26 18:41:01 by jgravalo         ###   ########.fr       */
+/*   Updated: 2023/08/26 20:03:54 by jgravalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,11 @@ char	*meta_str(char const *s, char c, int *n)
 	if (*s == '?' && ++s)
 		i++;
 	else
-		while (*s && /**s != ' '*/
-			((*s >= 'A' && *s <= 'Z') ||
-			(*s >= 'a' && *s <= 'z') ||
-			(*s >= '0' && *s <= '9'))/* ||
-			*s == '\'')*/ && ++i)
-			/**s != '\"' && */ 
-		{
-//			if (*s == '\'' && ++s && ++i)
-//				while (*s && *s != '\'' && ++i)
-//					s++;
+		while (*s && ((*s >= 'A' && *s <= 'Z')
+			|| (*s >= 'a' && *s <= 'z')
+			|| (*s >= '0' && *s <= '9'))
+			&& ++i)
 			s++;
-		}
 	new = ft_substr(s - i, 0, i);
 	*n = i;
 	return (new);
@@ -46,8 +39,7 @@ int	words_meta(const char *s, char c)
 	while (*s)
 	{
 		if (*s != c)
-		{
-			while (*s && *s != c)
+			while (*s && *s != c && ++j)
 			{
 				if (*s == '\'' && ++s)
 				{
@@ -58,12 +50,25 @@ int	words_meta(const char *s, char c)
 				}
 				s++;
 			}
-			j++;
-		}
 		else
 			++s;
 	}
 	return (j);
+}
+
+int search_dollar(char const *s, char c)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != c && s[i] != '\0')
+	{
+		if (s[i] == '\'' && ++i)
+			while (s[i] && s[i] != '\'')
+				i++;
+		i++;
+	}
+	return (i);
 }
 
 char	**ft_split_meta(char const *s, char c)
@@ -72,34 +77,27 @@ char	**ft_split_meta(char const *s, char c)
 	int		i;
 	char	**res;
 
-	///printf("count meta = %d\n", words_meta(s, c));
+//	printf("count meta = %d\n", words_meta(s, c));
 	res = (char **) malloc((words_meta(s, c) * (sizeof(char *))) + 8);
 	if (!res || !s)
 		return (0);
 	j = 0;
 	while (*s)
 	{	
-		//printf("*s es %c\n", *s);
-		while (*s != c && *s != '\0')
-		{
-//			++s;
-			if (*s == '\'' && ++s)
-				while (*s && *s != '\'')
-					s++;
-			++s;
-		}
+//		printf("*s es %c\n", *s);
+		s += search_dollar(s, c);
 		if (*s == '\0')
 			break;
 		++s;
 //		printf("*s es %c\n", *s);
-		//printf("*s es %s\n", s);
+//		printf("*s es %s\n", s);
 		if (*s != c)
 		{
 			i = 0;
 			res[j++] = meta_str(s, c, &i);
-			//printf("substr is %s, i es %d\n", res[j], i);
+//			printf("substr is %s, i es %d\n", res[j], i);
 			s += i;
-			//printf("*s es %s\n", s);
+//			printf("*s es %s\n", s);
 		}
 		else
 			++s;
@@ -108,19 +106,46 @@ char	**ft_split_meta(char const *s, char c)
 	return (res);
 }
 
+void	make_p(t_shell *shell, t_var *p, char *new_line, int n)
+{
+	int j;
+
+	j = 0;
+	if (p->tmp[n][j] == '?' && ++j) //exit code
+		p->exp = ft_itoa(shell->exit);
+	else
+	{	
+		if (search_var_line(p->tmp[n], shell->envp) == NULL)
+		{	
+			p->var = ft_strdup("");
+			p->exp = ft_strdup("");
+			j += ft_strlen(p->tmp[n]);
+		}
+		else
+		{
+			while (p->tmp[n][j] && ((p->tmp[n][j] >= 'a' && p->tmp[n][j] <= 'z')
+				|| (p->tmp[n][j] >= 'A' && p->tmp[n][j] <= 'Z')
+				|| (p->tmp[n][j] >= '0' && p->tmp[n][j] <= '9')))
+				j++;
+			p->var = ft_substr(p->tmp[n], 0, j);
+			p->exp = search_var_line(p->var, shell->envp);
+		}
+	}
+}
+
 static char	*get_var(t_shell *shell, t_var *p, char *new_line, int n)
 {
 	int		j;
 	int 	i;
 	int start;
 
-	//printf("ptmp0 es %s y len es %ld\n, new line es %s, y len de new line es %ld\n", p->tmp[n], ft_strlen(p->tmp[n]), new_line, ft_strlen(new_line));
+//	printf("ptmp0 es %s y len es %ld\n, new line es %s, y len de new line es %ld\n", p->tmp[n], ft_strlen(p->tmp[n]), new_line, ft_strlen(new_line));
 	i = 0;
 	start = 0;
 	while (new_line[i] && p->tmp[n])
 	{	
-		//printf("start is %c and i es %d\n", new_line[start], i);
-		//printf("newline before = <<%s>>\n", new_line + i);
+//		printf("start is %c and i es %d\n", new_line[start], i);
+//		printf("newline before = <<%s>>\n", new_line + i);
 		while (new_line[i] != '$')
 		{
 			if (new_line[i] == '\'' && ++i)
@@ -128,49 +153,26 @@ static char	*get_var(t_shell *shell, t_var *p, char *new_line, int n)
 					i++;
 			i++;
 		}
-		//printf("newline after = <<%s>>\n", new_line + i);
-		//printf("entra aqui, n es %d\n", n);
-		j = 0;
-		if (p->tmp[n][j] == '?') //exit code
-		{	
-			j++;
-			p->exp = ft_itoa(shell->exit);
-		}
-		else
-		{	
-			if (search_var_line(p->tmp[n], shell->envp) == NULL)
-			{	
-				p->var = ft_strdup("");
-				p->exp = ft_strdup("");
-				j += ft_strlen(p->tmp[n]);
-			}
-			else
-			{
-				while (p->tmp[n][j] &&
-					((p->tmp[n][j] >= 'a' && p->tmp[n][j] <= 'z') ||
-					(p->tmp[n][j] >= 'A' && p->tmp[n][j] <= 'Z') ||
-					(p->tmp[n][j] >= '0' && p->tmp[n][j] <= '9')))
-					j++;
-				p->var = ft_substr(p->tmp[n], 0, j);
-				p->exp = search_var_line(p->var, shell->envp);
-			}
-		}
-		//printf("p->exp es %s\n", p->exp);
-		//printf("start is %d, and i is %d, before substr\n", start, i);
-		//printf("newline substr is %s\n", ft_substr(new_line, start, i));
+//		printf("newline after = <<%s>>\n", new_line + i);
+//		printf("entra aqui, n es %d\n", n);
+		make_p(shell, p, new_line, n);
+//		printf("p->exp es %s\n", p->exp);
+//		printf("start is %d, and i is %d, before substr\n", start, i);
+//		printf("newline substr is %s\n", ft_substr(new_line, start, i));
 		p->c = ft_strjoin(ft_substr(new_line, start, i - start), p->exp);
-		//printf("p->c es %s\n", p->c);
+//		printf("p->c es %s\n", p->c);
 		p->new = ft_strjoin(p->new, p->c);
 		/* printf("p->c es %s\n", p->c);
 		p->tmp[n] += j;
 		p->new = ft_strjoin(p->c, p->tmp[n]); */
-		//printf("new line after getvar loop is %s\n", p->new);
+//		printf("new line after getvar loop is %s\n", p->new);
 		i += ft_strlen(p->tmp[n]) + 1;
 		start = i;
 		n++;
 	}
-	p->new = ft_strjoin(p->new, ft_substr(new_line, i, ft_strlen(new_line) - i));
-	//printf("new line after everything is %s\n", p->new);
+	p->new = ft_strjoin(p->new,
+		ft_substr(new_line, i, ft_strlen(new_line) - i));
+//	printf("new line after everything is %s\n", p->new);
 	return (p->new);
 }
 
@@ -181,31 +183,24 @@ static int is_there_dollar(char *line, char c)
 	i = 0;
 	while (line[i])
 	{	
-		if (line[i] == '\"')
+		if (line[i] == '\"' && ++i)
 		{
-			i++;
 			if (line[i] == '$' && line[i + 1] != ' ' && line[i + 1] != '\"')
 				return (1);
-			while (line[i] != '\"')
-			{
-				i++;
+			while (line[i] != '\"' && ++i)
 				if (line[i] == '$' && line[i + 1] != ' ' && line[i + 1] != '\"')
 					return (1);
-			}
 		}
-		else if (line[i] == '\'')
-		{
-			i++;
+		else if (line[i] == '\'' && ++i)
 			while (line[i] != '\'')
 				i++;
-		}
-		else if (line[i] == c && line[i + 1] != ' ' && line[i + 1] != '\0' && line[i + 1] != '\"')
+		else if (line[i] == c && line[i + 1] != ' '
+			&& line[i + 1] != '\0' && line[i + 1] != '\"')
 			return (1);
 		i++;
 	}
 	return (0);
 }
-
 static int check_for_tilde(char *line)
 {
 	int i;
@@ -213,21 +208,17 @@ static int check_for_tilde(char *line)
 	i = 0;
 	while (line[i] != '\0')
 	{	
-		if (line[i] == '\'')
+		if (line[i] == '\'' && ++i)
 		{	
-			i++;
 			while (line[i] != '\'')
 				i++;
 		}
-		else if (line[i] == '\"')
-		{	
-			i++;
+		else if (line[i] == '\"' && ++i)
 			while (line[i] != '\"')
 				i++;
-				
-		}
-		else if (line[i] == '~' && ((i == 0 && (line[i + 1] == '\0' || line[i + 1] == ' '))
-				 || (line[i - 1] == ' ' && (line[i + 1] == ' ' || line[i +1] == '\0'))))
+		else if (line[i] == '~' && ((i == 0 && (line[i + 1] == '\0'
+			|| line[i + 1] == ' ')) || (line[i - 1] == ' '
+			&& (line[i + 1] == ' ' || line[i + 1] == '\0'))))
 			return (1);
 		i++;
 	}
@@ -256,8 +247,12 @@ static char  *expand_tilde(t_shell *shell, char *new_line, char **envp, t_var *p
 		c = 0;
 		while (shell->readline [i])
 		{	
-			if (shell->readline[i] == '~' && (i == 0 && (shell->readline[i + 1] == '\0' || shell->readline[i + 1] == ' '))
-				 || (shell->readline[i - 1] == ' ' && (shell->readline[i + 1] == ' ' || shell->readline[i +1] == '\0')))
+			if (shell->readline[i] == '~' && (i == 0
+				&& (shell->readline[i + 1] == '\0'
+				|| shell->readline[i + 1] == ' '))
+				|| (shell->readline[i - 1] == ' '
+				&& (shell->readline[i + 1] == ' '
+				|| shell->readline[i +1] == '\0')))
 			{	
 				//printf("entra en tilde correcta\n");
 				tmp1 = ft_substr(new_line, 0, j);
