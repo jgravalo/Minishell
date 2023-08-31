d94cec68cc5ab6b954b3ea3b62f7307950e71f59
theonewhoknew <theonewhoknew@protonmail.com>
2023-08-31 11:19:20 +0200

    hacemos check para determinar si numero es inferior a LONG MIN para error pertinente de built in exit

--- src/built-ins/exit.c
+++ src/built-ins/exit.c
@@ -14,6 +14,20 @@ static int is_it_numeric(char *line)
 	return (0);
 }
 
+static int check_long_min(int64_t number)
+{	
+	uint64_t u_number;
+	uint64_t min;
+
+	min = (uint64_t) LONG_MIN * -1;
+	u_number = number * -1;
+	if (u_number > LONG_MIN)
+		return (1);
+	else
+		return (0);
+
+}
+
 void ft_exit(t_shell *shell, int n)
 {	
 	int64_t argument;
@@ -25,20 +39,23 @@ void ft_exit(t_shell *shell, int n)
 		if (shell->struct_cmd[n]->args[1][0] != '-')
 		{	
 			u_argument = ft_u_atoi(shell->struct_cmd[n]->args[1]);
-			if (u_argument > LONG_MAX)
+			if (u_argument > LONG_MAX || is_it_numeric(shell->struct_cmd[n]->args[1]) == 0)
 			{
 				write(2, "exit\n", 6);
 				write(2, "bash: exit: numeric argument required\n", 39);
 				exit(2);
 			}
 		}
-		if (argument < LONG_MIN || is_it_numeric(shell->struct_cmd[n]->args[1]) == 0)
-		{	
-			write(2, "exit\n", 6);
-			write(2, "bash: exit: numeric argument required\n", 39);
-			exit(2);
+		else
+		{
+			if (check_long_min(argument) || is_it_numeric(shell->struct_cmd[n]->args[1]) == 0)
+			{	
+				write(2, "exit\n", 6);
+				write(2, "bash: exit: numeric argument required\n", 39);
+				exit(2);
+			}
 		}
-		else if (shell->struct_cmd[n]->args[2] != NULL)
+		if (shell->struct_cmd[n]->args[2] != NULL)
 		{	
 			write(2, "exit", 5);
 			write(2, "bash: exit: too many arguments\n", 32);
